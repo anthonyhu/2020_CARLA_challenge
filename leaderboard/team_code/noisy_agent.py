a1655ee20da90dfaee6d0757b007dcddaa11a121
Anthony Hu <anth.hu@gmail.com>
2021-08-24 15:45:36 +0200

    Collect data on noisy agent, train world model on random actions

---
+++ leaderboard/team_code/noisy_agent.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+from team_code.auto_pilot import AutoPilot
+
+NOISE_LEVEL = 0.1
+
+
+def get_entry_point():
+    return 'NoisyAgent'
+
+
+class NoisyAgent(AutoPilot):
+    def _get_control(self, target, far_target, tick_data, _draw):
+        steer, throttle, brake, target_speed = super()._get_control(target, far_target, tick_data, _draw)
+
+        steer += NOISE_LEVEL * np.random.randn()
+        steer = np.clip(steer, -1.0, 1.0)
+
+        throttle += NOISE_LEVEL * np.random.randn()
+        throttle = np.clip(throttle, -1.0, 1.0)
+
+        return steer, throttle, brake, target_speed
